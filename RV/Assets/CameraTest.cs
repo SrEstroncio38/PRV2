bd78913a3b4cf99cdf0f999b1ea7527934cc91f7
David Fontela <d.fontela.1998@gmail.com>
2020-05-08 00:56:15 +0200

    Por dios va la camara

---
+++ RV/Assets/CameraTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+#endif
+
+public class CameraTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    #if PLATFORM_ANDROID
+            if (!Permission.HasUserAuthorizedPermission(Permission.Microphone) && !Permission.HasUserAuthorizedPermission(Permission.Camera))
+            {
+                Permission.RequestUserPermission(Permission.Microphone);
+                Permission.RequestUserPermission(Permission.Camera);
+        }
+    #endif
+    }
+}
