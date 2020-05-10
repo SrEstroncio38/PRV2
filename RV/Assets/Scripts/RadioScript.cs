39e41b991e8fe602acf6f0c9fd335de4b064ea59
David Fontela <d.fontela.1998@gmail.com>
2020-05-10 13:41:59 +0200

    FixedRadio
    Arreglados los bobtones de la radio

--- RV/Assets/Scripts/RadioScript.cs
+++ RV/Assets/Scripts/RadioScript.cs
@@ -9,6 +9,7 @@ public class RadioScript : MonoBehaviour
 
     public List<AudioClip> Songs;
     private int nextSong = 1;
+    private int currentS = 0;
     private float volume = 0;
     public TMP_Text currentSong;
     
@@ -32,6 +33,7 @@ public class RadioScript : MonoBehaviour
         string[] str = radio.clip.name.Split('-');
         currentSong.text = "<#ffffff>" + str[0] + "</color> - " + "<#40a0ff>" + str[1] + "</color>";
         nextSong++;
+        currentS = nextSong - 1;
         if (nextSong >= Songs.Count)
         {
             nextSong = 0;
@@ -44,12 +46,14 @@ public class RadioScript : MonoBehaviour
         Debug.Log("Anterior cancion");
         AudioSource radio = GetComponent<AudioSource>();
         radio.Stop();
-        nextSong--;
-        if(nextSong < 0)
+        currentS--;
+        nextSong = currentS + 1;
+        if(currentS < 0)
         {
-            nextSong = Songs.Count - 1;
+            currentS = Songs.Count - 1;
+            nextSong = 0;
         }
-        radio.clip = Songs[nextSong];
+        radio.clip = Songs[currentS];
         radio.Play();
         string[] str = radio.clip.name.Split('-');
         currentSong.text = "<#ffffff>" + str[0] + "</color> - " + "<#40a0ff>" + str[1] + "</color>";
