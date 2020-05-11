2d6a1c99b6cc5163af86314c7cbefa114a5c6380
Axel <axelsax1998@gmail.com>
2020-05-11 20:44:28 +0200

    Caminos confirmados <3

--- RV/Assets/Scripts/followPath.cs
+++ RV/Assets/Scripts/followPath.cs
@@ -19,6 +19,8 @@ public class followPath : MonoBehaviour
     public float keepSpeed = 0f;
     public float signSpeed = 0f;
     [SerializeField]
+    public float velocityStep = 0f;
+    [SerializeField]
     private float turnSpeed = 0f;
     [SerializeField]
     private float maxSpeedForw = 0f;
@@ -91,13 +93,13 @@ public class followPath : MonoBehaviour
 
     public void PressForward()
     {
-        moveSpeed += 1;
+        moveSpeed += velocityStep;
         if (moveSpeed > maxSpeedForw) moveSpeed = maxSpeedForw;
     }
 
     public void PressBackward()
     {
-        moveSpeed -= 1;
+        moveSpeed -= velocityStep;
         if (moveSpeed < -maxSpeedBack) moveSpeed = -maxSpeedBack;
     }
 
