9449f89322443d2c67c2abd6ae28517aa4cc3259
Axel <axelsax1998@gmail.com>
2020-05-12 18:05:26 +0200

    Mando puesto bonito

--- RV/Assets/Scripts/ButtonS/MovementButton.cs
+++ RV/Assets/Scripts/ButtonS/MovementButton.cs
@@ -23,16 +23,29 @@ public class MovementButton : ButtonScript
         else if (dir == direccion.Backwards)
             vagoneta.PressBackward();
         else if (dir == direccion.Left)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color(0.35f, 0, 0.5f);
             vagoneta.goLeft = true;
+        }
         else if (dir == direccion.Right)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color(0.35f, 0, 0.5f);
             vagoneta.goRight = true;
+        }
     }
 
     public override void InactiveAction()
     {
         if (dir == direccion.Left)
+        {
             vagoneta.goLeft = false;
+        }
         else if (dir == direccion.Right)
+        {
             vagoneta.goRight = false;
+        }
+
+        gameObject.GetComponent<Renderer>().material.color = new Color(0.96f, 0.26f, 0.21f);
+
     }
 }
